748b6db4ae6ade7457f7ced5754807e258f48301
Zakaria "LeMASHUP" Dutertre <dutertre.zakaria@gmail.com>
2023-11-14 10:20:38 +0100

    PlayerMovement V2
    I decided to redo the playermovement for making the local multiplayer possible

--- Assets/Scripts/Controller/PlayerMovement.cs
+++ Assets/Scripts/Controller/PlayerMovement.cs
@@ -1,85 +1,32 @@
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public InputManager inputManager;
-    public Rigidbody rb;
-    public float speed = 10;
-    public float jumpForce = 200;
-    public int dashSpeed = 10;
-    public Vector3 move;
-    private int indexJump = 0;
-    [SerializeField] private string inputNameHorizontal;
-    [SerializeField] private string inputNameVertical;
-
-    private bool isGrounded;
-
+    PlayerInput input;
+    InputAction moveAction;
+    InputAction jumpAction;
+    [SerializeField] float speed = 5;
+    private GameObject body;
+    // Start is called before the first frame update
     void Start()
     {
-        inputManager.inputMaster.Movement.Jump.started += _ => Jump();
-        //inputManager.inputMaster.Movement.Crouch.started += _ => Crouch();
-        //inputManager.inputMaster.Movement.Crouch.canceled += _ => CancelCrouch();
+        PlayerInput input = GetComponent<PlayerInput>();
+        moveAction = input.actions.FindAction("Move");
+        jumpAction = input.actions.FindAction("Jump");
+        body = transform.Find("Body").gameObject;
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        float forward = inputManager.inputMaster.Movement.Forward.ReadValue<float>();
-        move = transform.right * forward;
-        move *= speed;
-        rb.velocity = new Vector3(move.x, rb.velocity.y, move.z);
-
-        if (forward > 0)
-        {
-            gameObject.transform.Find("Body").transform.rotation = Quaternion.LookRotation(Vector3.forward);
-        }
-        else if (forward < 0)
-        {
-            gameObject.transform.Find("Body").transform.rotation = Quaternion.LookRotation(Vector3.back);
-        }
-    }
- /*
-    private void Crouch()
-    {
-        gameObject.GetComponent<Collider>().enabled = false;
+        MovePlayer();
     }
 
-    private void CancelCrouch()
+    private void MovePlayer()
     {
-       gameObject.GetComponent<Collider>().enabled = true;
-    }
-
- */
-    private void Jump()
-    {
-        if (isGrounded || indexJump == 0)
-        {
-            rb.velocity = Vector3.zero;
-            rb.AddForce(Vector3.up * jumpForce);
-            if (indexJump == 0)
-            {
-                indexJump = 1;
-            }
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.transform.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        if (collision.transform.CompareTag("Ground"))
-        {
-            isGrounded = false;
-            if (indexJump == 1)
-            {
-                indexJump = 0;
-            }
-        }
+        float roty = body.transform.rotation.eulerAngles.y;
+        Vector2 direction = moveAction.ReadValue<Vector2>();
+        transform.position += new Vector3(direction.x, 0, direction.y) * speed *Time.deltaTime;
     }
 }
