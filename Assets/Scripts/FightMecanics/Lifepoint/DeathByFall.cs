5881af4141ac271c96283d02b0a6c70195a859b6
Zakaria "LeMASHUP" Dutertre <dutertre.zakaria@gmail.com>
2023-11-15 14:50:19 +0100

    Update DeathByFall.cs

--- Assets/Scripts/FightMecanics/Lifepoint/DeathByFall.cs
+++ Assets/Scripts/FightMecanics/Lifepoint/DeathByFall.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DeathByFall : MonoBehaviour
 {
     [SerializeField] private LifePointManager lifePointManager;
 
-    private void Start()
+    private void Awake()
     {
         lifePointManager = GetComponent<LifePointManager>();
     }
     void Update()
     {
-        if (transform.position.y == -14.02)
+        if (transform.position.y <= -14.02)
         {
             lifePointManager.lifePoint = 0;
         }
