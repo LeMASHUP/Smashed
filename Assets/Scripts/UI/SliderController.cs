f0301fc59c28f0c8304570f87579c12a957fdd45
Zakaria "LeMASHUP" Dutertre <dutertre.zakaria@gmail.com>
2023-11-16 13:53:22 +0100

    Update SliderController.cs

--- Assets/Scripts/UI/SliderController.cs
+++ Assets/Scripts/UI/SliderController.cs
@@ -11,6 +11,8 @@ public class SliderController : MonoBehaviour
     void Start()
     {
         slider = GetComponent<Slider>();
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        gameObject.transform.GetChild(1).gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -24,6 +26,8 @@ public class SliderController : MonoBehaviour
         if (player != null)
         {
             slider.value = player.GetComponent<LifePointManager>().lifePoint;
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            gameObject.transform.GetChild(1).gameObject.SetActive(true);
         }
     }
 }
